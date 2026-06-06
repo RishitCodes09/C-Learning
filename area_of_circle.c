8c81c287aa73358f26f5ac4ecd6f07087df3bcc6
RishitCodes09 <rishitsingh123vis@gmail.com>
2026-06-06 13:40:52 +0530

    area_of_circle.c

---
+++ area_of_circle.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    
+float radius;
+float pi = 3.14;
+ 
+printf("radius of circle is");
+scanf("%f", &radius);
+
+printf("Area of circle is: %f", pi*radius*radius);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    return 0;
+}
